01e808939cb30213665a3d39575e78a8f93261a5
gregorthebigmac <gregorthebigmac>
2020-02-06 12:49:50 -0600

    added config files and a place for binaries

--- src/rpi_watch_dog.cpp
+++ src/rpi_watch_dog.cpp
@@ -1,1 +1,10 @@
-#include "../command/command.hpp"
+#include "command/command.hpp"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int  main() {
+    cout << "test" << endl;
+    return 0;
+}
