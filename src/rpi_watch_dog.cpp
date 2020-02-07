0e6b842934357f5e0ac8af3c5ac220374241f60f
gregorthebigmac <gregorthebigmac>
2020-02-07 16:03:58 -0600

    got a decent baseline for load_settings

--- src/rpi_watch_dog.cpp
+++ src/rpi_watch_dog.cpp
@@ -1,10 +1,15 @@
 #include "command/command.hpp"
+#include "load_settings.hpp"
 
 using std::cout;
 using std::endl;
 using std::string;
 
+command cmd;
+load_settings settings;
+
 int  main() {
-    cout << "test" << endl;
+    settings.toggle_debug_mode();
+    settings.load_config_file();
     return 0;
 }
